dfad9e6f017697342c4032fa72faed49190e670d
Allan Fel <allan.goioere@hotmail.com>
2022-05-27 20:07:07 -0300

    Update app.js

--- 404/app.js
+++ 404/app.js
@@ -4,6 +4,9 @@ $(document).ready(function () {
         const r = Math.floor(Math.random() * (count));
         const res = Object.values(data.photos)[r];
         $("#mais").attr("href", "http://pixiv.net/artworks/" + res[0])
-        $('#img-background').css("background-image", "url( 'https://43d.github.io/404/image/"+res[0] + "_p" + res[1] + ".jpg"+"')"); 
+        const url = "https://43d.github.io/404/image/"+res[0] + "_p" + res[1] + ".jpg";
+        $('#img-background').css("background-image", "url( '"+url+"')"); 
+        $("meta")[12].content =url;
+        $("meta")[17].content =url;
     });
 });
